9f295a50643a7053b4cf39da666a28bff858bc00
Piyush <zephyrnebulynx@gmail.com>
2024-09-10 22:16:02 +0530

    first project

---
+++ color_changer/color_change.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) =>{
+        body.style.backgroundColor = e.target.id;
+    })
+})
