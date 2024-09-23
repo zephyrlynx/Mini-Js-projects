15937668591d1522c5457011a44d6f1e8e7932e4
Piyush <zephyrnebulynx@gmail.com>
2024-09-23 22:59:00 +0530

    twelfth project

---
+++ Mini_Calender/script.js
@@ -0,0 +1,42 @@
+const date = document.getElementById('date');
+const day = document.getElementById('day');
+const month = document.getElementById('month');
+const year = document.getElementById('year');
+
+const today = new Date();
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+date.innerHTML = (today.getDate() < 10 ? '0' + today.getDate() : today.getDate());
+day.innerHTML = days[today.getDay()];
+month.innerHTML = monthNames[today.getMonth()];
+year.innerHTML = today.getFullYear();
+
+
+
+
+
+
